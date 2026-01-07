0e55b10f68b2fa82975f335ee56aa63eaea1f534
Ola Lømo Ellingsen <ola.l.ellingsen@gmail.com>
2026-01-07 10:00:10 +0100

    bug in concertsblock

--- next/components/ConcertsBlock.tsx
+++ next/components/ConcertsBlock.tsx
@@ -9,19 +9,21 @@ export default function ConcertsBlock({
   concertList: Concert[];
   title?: string;
 }) {
-  return (
-    <>
-      <h2>{title}</h2>
-      <ul className="w-full flex flex-row gap-2 md:gap-4 overflow-x-auto no-scrollbar overflow-y-hidden snap-x snap-mandatory no-scrollbar scroll-smooth lg:grid lg:grid-cols-3">
-        {concertList.map((concert) => (
-          <li
-            key={concert._id}
-            className="bg-primary text-background dark:text-foreground min-w-9/10 w-full sm:min-w-4/10 snap-start"
-          >
-            <ConcertCard concert={concert} />
-          </li>
-        ))}
-      </ul>
-    </>
-  );
+  if (concertList) {
+    return (
+      <>
+        <h2>{title}</h2>
+        <ul className="w-full flex flex-row gap-2 md:gap-4 overflow-x-auto no-scrollbar overflow-y-hidden snap-x snap-mandatory no-scrollbar scroll-smooth lg:grid lg:grid-cols-3">
+          {concertList.map((concert) => (
+            <li
+              key={concert._id}
+              className="bg-primary text-background dark:text-foreground min-w-9/10 w-full sm:min-w-4/10 snap-start"
+            >
+              <ConcertCard concert={concert} />
+            </li>
+          ))}
+        </ul>
+      </>
+    );
+  }
 }
