1d5c3a4214d44fb3c26a53478cd9fd23f73472ae
Ola Lømo Ellingsen <ola.l.ellingsen@gmail.com>
2026-01-07 16:40:31 +0100

    sanity token

--- next/sanity/client.ts
+++ next/sanity/client.ts
@@ -6,6 +6,8 @@ export const client = createClient({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
   apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION!,
+  token: process.env.SANITY_API_TOKEN,
+  perspective: "published",
   useCdn: false,
 });
 
